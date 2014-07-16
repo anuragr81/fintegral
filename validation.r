9ab4d7646966daeaf9e5ded2119c3a9a29860eba
Your Name <you@example.com>
2014-07-16 17:04:50 +0100

    Replaced cumprod with cumsum.

--- validation.r
+++ validation.r
@@ -33,11 +33,11 @@ bscallprice <- function(S_0,K,r_f,vol,t,T){
 generate_path <- function (S_0,r_f,vol,dt,T){
     t=0
     n=T/dt;
-    z=rnorm(n);
-    vec=exp((r_f-vol*vol/2)*dt +vol*z*sqrt(dt))
-    vec[1]=S_0
-    values=cumprod(vec)
+    z=rnorm(n-1);
     tarray=seq(0,n-1)*dt
+    vec=cumsum((r_f-vol*vol/2)*dt +vol*z*sqrt(dt))
+    values=S_0*exp(vec)
+    values=c(S_0,values) # appending initial value
     return (data.frame(values=values,t=tarray));
 }
 
