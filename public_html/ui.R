d19660c5f05e2407874532901bed18261e8b0be2
Your Name <you@example.com>
2014-07-28 23:16:36 +0100

    Adding controls.

--- public_html/ui.R
+++ public_html/ui.R
@@ -2,40 +2,30 @@ library(shiny)
 library(ggplot2)
 
 
-dataset <- diamonds
-
 shinyUI(fluidPage(
   titlePanel("PNL Hedging"),
   fluidRow(
+    
+    numericInput("S_0", "Current Underlying Price",
+                 100, min = 0, max = 1000, step = .01),
+    numericInput("K", "Strike Price",
+                 100, min = 0, max = 1000, step = .01),
+    numericInput("T", "Time until Maturity",
+                 1, min = 0, max = 1, step = .01),
   
+  
+    sliderInput("vol", 
+                label = "Volatilty:",
+                min = 0, max = 1, value = .05, step=.01),
+    
+    sliderInput("dt", 
+                label = "Step Size:",
+                min = 0, max = 1, value = .05, step=.01),
+    
+    sliderInput("r_f", 
+                label = "RiskFree Rate:",
+                min = 0, max = 1, value = .05, step=.01),
+    
            mainPanel(plotOutput("data"))
            
 )))
-
-if(FALSE){
-shinyUI(pageWithSidebar(
-
-  headerPanel("Diamonds Explorer"),
-
-  sidebarPanel(
-
-    sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
-                value=min(1000, nrow(dataset)), step=500, round=0),
-
-    selectInput('x', 'X', names(dataset)),
-    selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
-    selectInput('color', 'Color', c('None', names(dataset))),
-    
-    checkboxInput('jitter', 'Jitter'),
-    checkboxInput('smooth', 'Smooth'),
-
-    selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
-    selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
-  ),
-
-  mainPanel(
-    plotOutput('map')
-  )
-))
-
-}
