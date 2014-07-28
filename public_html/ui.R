fef78368ded8184e1b5d3cf02c56a50957a567fd
Your Name <you@example.com>
2014-07-28 23:38:16 +0100

    Adding amivest,transaction-cost controls.

--- public_html/ui.R
+++ public_html/ui.R
@@ -11,9 +11,12 @@ shinyUI(fluidPage(
     numericInput("K", "Strike Price",
                  100, min = 0, max = 1000, step = .01),
     numericInput("T", "Time until Maturity",
-                 1, min = 0, max = 1, step = .01),
-  
-  
+                 10, min = 0, max = 100, step = .01),
+    numericInput("at", "Amivest Ratio",
+                 .5, min = 0, max = 100, step = .01),
+    numericInput("tc", "Transaction costs (fraction)",
+                 1, min = 0, max = 100, step = .01),
+    
     sliderInput("vol", 
                 label = "Volatilty:",
                 min = 0, max = 1, value = .05, step=.01),
