565607d57b148efdf00240b989adc802e1f6ff8e
Your Name <you@example.com>
2014-07-30 22:25:45 +0100

    Fixing AT problem

--- validation.r
+++ validation.r
@@ -3,236 +3,71 @@
 #source("file://C:/Users/anuragr/Desktop/model_validation/validation.r")
 # Reiner & Rubinstein Barrier Paper
 
-test <- function(K) {
-    ds=.1;
-    finS=3*K;
-    veclen=as.integer(finS/ds);
-    S_0=seq(0,finS,ds); S_0=S_0[1:length(S_0)-1];
-    K=rep(K,veclen);
-    #r_f=seq(0,.05,(.05/veclen)); r_f=r_f[1:(length(r_f)-1)];
-    r_f=rep(.05,veclen);
-    vol=rep(.6,veclen);
-    t=rep(0,veclen);
-    T=rep(1,veclen);
-    v=bscallprice(S_0,K,r_f,vol,t,T);
-    plot(S_0,v$gamma); #,S_0,v$delta,S_0,v$gamma);
-}
-
-bscallprice <- function(S_0,K,r_f,vol,t,T){
-    numerator=log(S_0/K) + (r_f+(vol*vol)*0.5)*(T-t);
-    denominator=vol*sqrt(T-t);
-    d1=(numerator/denominator);
-    d2=d1-vol*sqrt(T-t);
-    res=list();
-    price=(S_0*pnorm(d1)-K*exp(-r_f*(T-t))*pnorm(d2));
-    delta=pnorm(d1);
-    gamma=dnorm(d1)/(S_0*vol*sqrt(T-t));
-    return (data.frame(price=price,delta=delta,gamma=gamma));
-}
-
-generate_path <- function (S_0,r_f,vol,dt,T){
-    t=0
-    n=T/dt;
-    z=rnorm(n-1);
-    tarray=seq(0,n-1)*dt
-    vec=cumsum((r_f-vol*vol/2)*dt +vol*z*sqrt(dt))
-    values=S_0*exp(vec)
-    values=c(S_0,values) # appending initial value
-    return (data.frame(values=values,t=tarray));
-}
-
-#========== BARRIER PRICER ============
-
-value_downandout_func <- function(S,K,B){
-# if S shoots up above B payoff is S_T-K
-   if (min(S)<B){
-               return(0);
-   }else{
-               return(max(S[length(S)]-K,0));
-   }
-}
-
-
-value_downandout <- function (npaths,S_0,K,B,r_f,vol,dt,T) {
-
-res=array(0);
-out=array(0);
-
-for ( i in seq(npaths)) {
-                paths=generate_path(S_0=S_0,r_f=r_f,vol=vol,dt=dt,T=T)
-		out[i]=value_downandout_func(S=paths$values,K=K,B=B);
-}
-
-res=(sum(out)/length(out))*exp(-r_f*T);
-sg_c=sd(out);
-
-values_left=res-1.96*sg_c/sqrt(npaths);
-values_right=res+1.96*sg_c/sqrt(npaths);
-
-return (data.frame(left=values_left,right=values_right));
-
-}
-
-num_stocks_to_short_zerodp <- function(underlying_price,tc,delta,nShortedStocks,dS){
-    # Check if x<0 or x>0 conditions apply. If neither works, return x=0 (don't hedge) and report error
-    numerator=(delta-nShortedStocks)*dS
-    if (numerator>0){
-       denominator=underlying_price+tc # always +ve
-       return (numerator/denominator)
-    } else {
-       denominator=underlying_price-tc # for -ve return values
-       if (denominator>0){
-            return(numerator/denominator); # -ve return value
-       } else {
-            print("Error: invalid hedge parameters");
-            return (0); # do nothing
-       }
-    }
-    #return ((1/(tc+underlying_price))*((delta-nShortedStocks)*dS));
-}
-
-num_stocks_to_short_zerodp_g <- function(underlying_price,tc,amivest,delta,nShortedStocks,dS){
-    # Check if x<0 or x>0 conditions apply. If neither works, return x=0 (don't hedge) and report error
-    #print(paste("S_t=",underlying_price,"n=",nShortedStocks,"amivest=",amivest,"tc=",tc))
-    if (amivest==0){
-       return(num_stocks_to_short_zerodp(underlying_price=underlying_price,tc=tc,delta=delta,nShortedStocks=nShortedStocks,dS=dS));
-    }
-
-    A  = 2*amivest
-    B1 = underlying_price-nShortedStocks*amivest+tc
-    D1 = (underlying_price-nShortedStocks*amivest+tc)^2-4*amivest*(delta-nShortedStocks)*dS; # x>0
-    B2 = underlying_price-nShortedStocks*amivest-tc
-    D2 = (underlying_price-nShortedStocks*amivest-tc)^2-4*amivest*(delta-nShortedStocks)*dS; # x<0
-    print(paste("D1=",D1))
-    print(paste("D2=",D2))
-
-    if ( (D1<0) && (D2<0) ){
-            print("Error: invalid hedge parameters (D1,D2) <0");
-            return (0); # do nothing
-    }
-    if (D1<0) {
-       # D2> 0, x<0
-       xO1=(B2+sqrt(D2))/A
-       xO2=(B2-sqrt(D2))/A
-       if (xO1<0){
-            if (xO2<0) {
-               return(max(xO1,xO2))
-            }
-            else {
-               return (xO1);
-            } 
-         } else {
-            if (xO2<0) {
-               return(xO2);
-            } else {
-               print("Error: hedging params (D2,xO)>0)")
-               return (0);
-            }
-         } 
-    }
-    if (D2<0) {
-       # D1>0, x>0
-       xT1=(B1+sqrt(D1))/A
-       xT2=(B1-sqrt(D1))/A
-       if (xT1>0){
-          if (xT2>0){
-                return(min(xT1,xT2));
-          } else {
-                return(xT1);
-          }
-       } else {
-          if (xT2>0){
-                return(xT2);
-          }else{
-               print("Error: hedging params (D2,xT)<0)")
-               return (0);
-          }
-       }  
-    }
-    #D1>0 and D2>0
-    xO1=(B2+sqrt(D2))/A
-    xO2=(B2-sqrt(D2))/A
-    xT1=(B1+sqrt(D1))/A
-    xT2=(B1-sqrt(D1))/A
-    sols=c(xO1,xO2,xT1,xT2)
-    positives=sols[sols>0]
-    negatives=sols[sols<0]
-    if (length(positives)>0){
-        return(min(positives))
-    } else {
-        return(max(negatives))
-    }
-    
-}
-
-
-num_stocks_to_short_deltas <- function(underlying_price,tc,delta,nxtdelta,nShortedStocks,dS){
-  return(nxtdelta-delta);
-}
-
-num_stocks_to_short_direct <- function(underlying_price,dP,nShortedStocks,dS){
-    return ((1/underlying_price)*(dP-nShortedStocks*dS));
-}
-
-show_deltas <- function(t,notc_deltas,notc_hedged_pos,tc_deltas,tc_hedged_pos) {
-    plot(0,0,xlab="Time", ylab="Delta, HedgedPosition(normalized)", xlim=c(0,max(t)),ylim=c(-2,3));
-    cl<-rainbow(2);
-    lines(t,notc_deltas,col=cl[1],lty=1)
-    lines(t,notc_hedged_pos/max(abs(notc_hedged_pos)),col=cl[2],lty=2);
-    lines(t,tc_hedged_pos/max(abs(tc_hedged_pos)),col=cl[3],lty=3);
-    legend(1,3,c("delta","hedged pos (normalized-noTC)","hedged pos(normalized-TC)"),col=cl, lty=c(1,2,3,4));
-}
-
-show_stock_opt <- function(path,option_prices,hedged_pos) {
-    plot(0,0,xlab="Time", ylab="Prices" , xlim=c(0,max(path$t)),ylim=c(-max(path$values),max(path$values)));
-    cl<-rainbow(3);
-    lines(path$t,option_prices,col=cl[1],lty=1)
-    lines(path$t,path$values,col=cl[2],lty=2);
-    lines(path$t,hedged_pos,col=cl[3],lty=3);
-    legend(1,-20,c("option","stock","hedged pos"),col=cl, lty=c(1,2,3));
-}
-
-
-pnl_value <- function(S_t,P_t,nShorted,tc) {
-   return (P_t-nShorted*S_t-abs(nShorted)*tc);
-}
-
-
+source("bspricers.r")
+source("hedgesteps.r")
+source("display.r")
 
 hedged_position <- function (path_t,path_values,r_f,vol,dt,T,K,tc,at) {
-    nh = length(path_values);
-    option_prices=array();
-    hedged_pos=array();
-    bs=bscallprice(S_0=path_values,K=K,r_f=r_f,vol=vol,t=path_t,T);
+  isPrint=FALSE;
+  nh = length(path_values);
+  option_prices=array();
+  hedged_pos=array();
+  bs=bscallprice(S_0=path_values,K=K,r_f=r_f,vol=vol,t=path_t,T);
+  
+  nShortedStocks = bs$delta[1];
+  hedged_pos[1] =  pnl_value(S_t=path_values[1],P_t=bs$price[1],nShorted=nShortedStocks,at=at,tc=tc)
 
-    nShortedStocks = bs$delta[1];
-    hedged_pos[1] =  pnl_value(S_t=path_values[1],P_t=bs$price[1],nShorted=nShortedStocks,tc=tc)
 
-    for ( i in seq (2,nh) ) {
-           dS= path_values[i] - path_values[i-1];
-           #nShort=num_stocks_to_short_deltas(tc=tc,underlying_price=path_values[i],delta=bs$delta[i-1],nxtdelta=bs$delta[i],nShortedStocks=nShortedStocks,dS=dS);
-           print(paste("nShortedStocks=",nShortedStocks))
-           nShort=num_stocks_to_short_zerodp_g(tc=tc,amivest=at,underlying_price=path_values[i],delta=bs$delta[i-1],nShortedStocks=nShortedStocks,dS=dS);
-           print(paste("Before: nShort=",nShort,"nShortedStocks=",nShortedStocks))
-           nShortedStocks = nShortedStocks + nShort;
-           print(paste("After: nShort=",nShort,"nShortedStocks=",nShortedStocks))
-           hedged_pos[i] = pnl_value(S_t=path_values[i],P_t=bs$price[i],nShorted=nShortedStocks,tc=tc);
-           #print(paste("price=",bs$price,"nshort=",nShort,"nShortedStocks=",nShortedStocks,"Position=",hedged_pos[i]));
-           #readline();
+  if (isPrint){
+    print(paste("hedged_pos[1]=",hedged_pos[1]));
+  }
+  
+  for ( i in seq (2,nh) ) {
+    dS= path_values[i] - path_values[i-1];
+    if (isPrint){
+      print(paste("dS=",dS,"nShortedStocks=",nShortedStocks))
     }
-    return(data.frame(hedged_pos=hedged_pos,t=path_t,deltas=bs$delta));
+    nShort=num_stocks_to_short_zerodp_g(tc=tc,amivest=at,underlying_price=path_values[i],delta=bs$delta[i-1],nShortedStocks=nShortedStocks,dS=dS);
+    if (isPrint){
+       print(paste("Before: nShort(to short)=",nShort,"nShortedStocks=",nShortedStocks))
+    }
+    nShortedStocks = nShortedStocks + nShort;
+    if (isPrint){
+       print(paste("After: nShort(shorted)=",nShort,"nShortedStocks=",nShortedStocks))
+    }
+    hedged_pos[i] = pnl_value(S_t=path_values[i],P_t=bs$price[i],nShorted=nShortedStocks,at=at,tc=tc);
+    if (isPrint){
+        print(paste("hedged_pos[",i,"]=",hedged_pos[i]));
+        print(paste("price=",bs$price[i],"nshort=",nShort,"nShortedStocks=",nShortedStocks,"Position=",hedged_pos[i]));
+    }
+  }
+  return(data.frame(hedged_pos=hedged_pos,t=path_t,deltas=bs$delta));
+}
+
+test <- function(K) {
+  ds=.1;
+  finS=3*K;
+  veclen=as.integer(finS/ds);
+  S_0=seq(0,finS,ds); S_0=S_0[1:length(S_0)-1];
+  K=rep(K,veclen);
+  #r_f=seq(0,.05,(.05/veclen)); r_f=r_f[1:(length(r_f)-1)];
+  r_f=rep(.05,veclen);
+  vol=rep(.6,veclen);
+  t=rep(0,veclen);
+  T=rep(1,veclen);
+  v=bscallprice(S_0,K,r_f,vol,t,T);
+  plot(S_0,v$gamma); #,S_0,v$delta,S_0,v$gamma);
 }
 
 simul <- function(calculate){
     S_0=50;
     vol=.4;
-    dt=.1;
-    T=10;
+    dt=.01;
+    T=1;
     K=50;
     r_f=.05;
-    tc=1;
+    tc=.2;
     at=1;
-
 
     nh=T/dt;
     vec_r_f=rep(r_f,nh)
@@ -242,7 +77,6 @@ simul <- function(calculate){
 
     if (calculate==0){
     # could be replaced with an IR model
-
         path = generate_path(S_0,r_f,vol,dt,T);
         hp_notc=hedged_position(path_t=path$t,path_values=path$values,r_f=vec_r_f,vol=vec_vol,dt=dt,T=T,K=K,tc=0,at=at);
         hp_tc=hedged_position(path_t=path$t,path_values=path$values,r_f=vec_r_f,vol=vec_vol,dt=dt,T=T,K=K,tc=tc,at=at);
@@ -263,26 +97,6 @@ simul <- function(calculate){
         #hist(out); 
         print(paste("mean-stdev(PNL):",mean(out)));
     }
-}
-
-show_barrier_opt_wrt_time <- function(t,callprice,calldelta,doutprice,doutdelta) {
-    plot(0,0,xlab="Time", ylab="Prices" , xlim=c(0,max(t)),ylim=c( min(0,min(doutdelta)), max(doutdelta)))
-    cl<-rainbow(4);
-    lines(t,callprice/max(callprice),col=cl[1],lty=1)
-    lines(t,calldelta,col=cl[2],lty=2);
-    lines(t,doutprice/max(doutprice),col=cl[3],lty=3);
-    lines(t,doutdelta,col=cl[4],lty=4);
-    legend(.5,.2,c("call_normalized","call-delta","barrier_normalized","barrier-delta"),col=cl, lty=c(1,2,3,4));
-}
-
-show_barrier_opt_wrt_stock <- function(S_t,callprice,calldelta,doutprice,doutdelta) {
-    plot(0,0,xlab="Underlying", ylab="Prices" , xlim=c(0,max(S_t)),ylim=c( min(0,min(calldelta)), max(calldelta)))
-    cl<-rainbow(4);
-    #lines(S_t,callprice/max(callprice),col=cl[1],lty=1)
-    lines(S_t,calldelta,col=cl[2],lty=2);
-    #lines(S_t,doutprice/max(doutprice),col=cl[3],lty=3);
-    lines(S_t,doutdelta,col=cl[4],lty=4);
-    #legend(S_t[1]/32,.6,c("call_normalized","call-delta","barrier_normalized","barrier-delta"),col=cl, lty=c(1,2,3,4));
 }
 
 
@@ -309,23 +123,6 @@ cbn=value_downandout_exp(S_0=S_t+dS,K=rep(K,nh),B=rep(B,nh),r_f=rep(r_f,nh),vol=
 delta=(cbn-cb)/dS
 show_barrier_opt_wrt_stock(S_t,c$price,c$delta,doutprice=cb,doutdelta=delta)
 
-}
-
-value_downandout_exp <- function(S_0,K,B,r_f,vol,t,T) {
-cbs=bscallprice(S_0=S_0,K=K,r_f=r_f,vol=vol,t=t,T);
-cbsi=bscallprice(S_0=(B*B/S_0),K=K,r_f=r_f,vol=vol,t=t,T=T);
-n1=cbs$price
-n2=((S_0/B)^(1-(2*r_f/(vol*vol)))*cbsi$price)
-return(n1-n2);
-#ts.plot(S_0,ts((S_0[1]^2)/S_0)) # <-- beauty
-
-}
-
-delta_downandout_exp <-function(S_0,K,B,r_f,vol,dt,T){
-   cbs=value_downandout_exp(S_0=S_0,K=K,B=B,r_f=r_f,vol=vol,dt=dt,T=T)
-   dS=.001;
-   cbsnext=value_downandout_exp(S_0=S_0+dS,K=K,B=B,r_f=r_f,vol=vol,dt=dt,T=T)
-   return ((cbsnext$price-cbs$price)/dS)
 }
 
 testBSStock <- function() {
