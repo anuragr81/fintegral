cdd5b8c1de44cb124c7a2fed29cf83824f679ac6
Anurag <anandmukh@yahoo.com>
2014-08-07 18:39:42 +0100

    Adding delta-logic back.

--- bspricers.r
+++ bspricers.r
@@ -10,8 +10,27 @@ generate_path <- function (S_0,r_f,vol,dt,T){
     return (data.frame(values=values,t=tarray));
 }
 
+checkargs_bscallpricer<- function(pricerArgs){
+  if(pricerArgs$tol<=0 && pricerArgs$tol<=1 ){
+    return(FALSE);
+  }
+  if (min(pricerArgs$K)<=0){
+    return(FALSE);
+  }
+  if (min(pricerArgs$r_f)<0){
+    return(FALSE);
+  }
+  if (min(pricerArgs$vol)<0){
+    return(FALSE);
+  }
+  if (min(pricerArgs$T)<=0){
+    return(FALSE);
+  }
+  return(TRUE);
+}
+
+
 bscallprice <- function(S_0,t,pricerArgs){
-  
     K=pricerArgs$K;
     r_f=pricerArgs$r_f;
     vol=pricerArgs$vol;
@@ -23,8 +42,6 @@ bscallprice <- function(S_0,t,pricerArgs){
     d2=d1-vol*sqrt(T-t);
     price=(S_0*pnorm(d1)-K*exp(-r_f*(T-t))*pnorm(d2));
     delta=pnorm(d1);
-    gamma=dnorm(d1)/(S_0*vol*sqrt(T-t));
-    
+    gamma=dnorm(d1)/(S_0*vol*sqrt(T-t));    
     return (data.frame(price=price,delta=delta,gamma=gamma));
 }
-
